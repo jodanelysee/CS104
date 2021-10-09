86386393ef57e74f8fc34097d6de37c0019f8f23
Jodan Elysee <75323379+jodanelysee@users.noreply.github.com>
2021-10-09 12:51:45 -0400

    average program

---
+++ average.py
@@ -0,0 +1,14 @@
+average = 0
+total = 0
+howManyEntered = 0
+howMany = int(input('How many test scores would you like to average: '))
+testScore = int(input('Enter test score: '))
+total = total + testScore
+howManyEntered = howManyEntered + 1
+while howManyEntered < howMany:
+    testScore = int(input('Enter test score: '))
+    total = total + testScore
+    howManyEntered = howManyEntered + 1
+average = total/howMany
+print("The average of the test scores you entered is: ")
+print(average)
